e3a62a230e659bd893b305d694aedb88f7a592c1
Dominic Chen <cchendominic@gmail.com>
2026-07-12 22:58:49 -0400

    Update the app with multiple models

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,11 +3,24 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class HeroBase(SQLModel):
     name: str = Field(index=True)
     age: int | None = Field(default=None, index=True)
+    
+class Hero(HeroBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     secret_name: str
+
+class HeroPublic(HeroBase):
+    id: int
+    
+class HeroCreate(HeroBase):
+    secret_name: str
+    
+class HeroUpdate(HeroBase):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -30,28 +43,41 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-@app.post("/heroes/")
-def create_hero(hero: Hero, session: SessionDep) -> Hero:
-    session.add(hero)
+@app.post("/heroes/", response_model=HeroPublic)
+def create_hero(hero: HeroCreate, session: SessionDep):
+    db_hero = Hero.model_validate(hero)
+    session.add(db_hero)
     session.commit()
-    session.refresh(hero)
-    return hero
+    session.refresh(db_hero)
+    return db_hero
 
-@app.get("/heroes/")
+@app.get("/heroes/", response_model=list[HeroPublic])
 def read_heroes(
-     session: SessionDep,
-     offset: int = 0,
-     limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
-     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-     return heroes
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
 
-@app.get("/heroes/{hero_id}")
-def read_hero(hero_id: int, session: SessionDep) -> Hero:
+@app.get("/heroes/{hero_id}", response_model=HeroPublic)
+def read_hero(hero_id: int, session: SessionDep):
     hero = session.get(Hero, hero_id)
     if not hero:
         raise HTTPException(status_code=404, detail="Hero not found")
     return hero
+
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+    return hero_db
 
 @app.delete("/heroes/{hero_id}")
 def delete_hero(hero_id: int, session: SessionDep):
