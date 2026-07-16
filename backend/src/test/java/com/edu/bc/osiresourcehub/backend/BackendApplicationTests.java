10265c0f3c56064b1b72e82d63827c5db83bbebe
Dominic Chen <cchendominic@gmail.com>
2026-07-16 11:01:55 -0400

    Scaffold Spring Boot project structure

---
+++ backend/src/test/java/com/edu/bc/osiresourcehub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.bc.osiresourcehub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
